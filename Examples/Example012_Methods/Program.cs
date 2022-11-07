813c830555552f4d7f3d604786b6e77a5434de73
Sergey <melnikov_s_a@mail.ru>
2022-11-07 05:01:21 +0300

    Example012 multiply

--- Examples/Example012_Methods/Program.cs
+++ Examples/Example012_Methods/Program.cs
@@ -65,11 +65,20 @@ int year = Method3();
 string Method4(int count, string text)
 {
     string result = String.Empty;
-    for (int i = 0; i < count; i++)    
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
     }
     return result;
 }
 string res = Method4(10, "z");
-Console.WriteLine(res);
+// Console.WriteLine(res);
+
+for (int i = 2; i < 11; i++)
+{
+    for (int j = 2; j < 11; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}

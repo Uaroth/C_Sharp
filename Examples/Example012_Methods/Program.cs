42b61cd2ccef464d1f68b5432a456306b927d4af
Sergey <melnikov_s_a@mail.ru>
2022-11-07 05:45:08 +0300

    Example012 error to remember

--- Examples/Example012_Methods/Program.cs
+++ Examples/Example012_Methods/Program.cs
@@ -74,11 +74,38 @@ string Method4(int count, string text)
 string res = Method4(10, "z");
 // Console.WriteLine(res);
 
-for (int i = 2; i < 11; i++)
+// for (int i = 2; i < 11; i++)
+// {
+//     for (int j = 2; j < 11; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            + "вы бы взяли приступом согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+// string s = "qwerty"
+//             0123
+// s{3} // r
+
+string Replace(string text, char oldValue, char newValue)
 {
-    for (int j = 2; j < 11; j++)
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
     {
-        Console.WriteLine($"{i} x {j} = {i * j}");
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
     }
-    Console.WriteLine();
+
+    return result;
 }
+
+string newText = Replace(text, " ", "|");
+Console.WriteLine(newText);

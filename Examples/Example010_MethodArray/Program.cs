bd4187e065d259df1df7a5a8a5424fabdcf805a2
Sergey <melnikov_s_a@mail.ru>
2022-11-07 03:05:24 +0300

    Example010 numbers

---
+++ Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 11, 2, 31, 4, 5, 6, 17, 8};
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+index++;
+}

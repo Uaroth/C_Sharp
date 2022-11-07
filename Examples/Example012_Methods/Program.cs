b33ffd4f6353a318e1999c4cccc56422c30bf06e
Sergey <melnikov_s_a@mail.ru>
2022-11-07 06:42:19 +0300

    Example012 SelectionSort

--- Examples/Example012_Methods/Program.cs
+++ Examples/Example012_Methods/Program.cs
@@ -84,28 +84,65 @@ string res = Method4(10, "z");
 // }
 
 
-string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
-            + "ежели бы вас послали вместо нашего милого Винценгероде, "
-            + "вы бы взяли приступом согласие прусского короля. "
-            + "Вы так красноречивы. Вы дадите мне чаю?";
+// string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            // + "ежели бы вас послали вместо нашего милого Винценгероде, "
+            // + "вы бы взяли приступом согласие прусского короля. "
+            // + "Вы так красноречивы. Вы дадите мне чаю?";
 
 
 // string s = "qwerty"
 //             0123
 // s{3} // r
 
-string Replace(string text, char oldValue, char newValue)
-{
-    string result = String.Empty;
-    int length = text.Length;
-    for (int i = 0; i < length; i++)
-    {
-        if (text[i] == oldValue) result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
-    }
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if (text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
 
-    return result;
+//     return result;
+// }
+
+// string newText = Replace(text, ' ' , '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace (newText, 'к' , 'К');
+// newText = Replace (newText, 'С' , 'с');
+// Console.WriteLine(newText);
+
+int [] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
 }
 
-string newText = Replace(text, ' ' , '|');
-Console.WriteLine(newText);
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array [minPosition]) minPosition = j;
+        }
+
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray (arr);
+SelectionSort(arr);
+
+PrintArray(arr);

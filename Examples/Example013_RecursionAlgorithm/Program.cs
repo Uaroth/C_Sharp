fcfb1857fcdfcff48223a93ed3673494162f83e7
Sergey <melnikov_s_a@mail.ru>
2022-11-07 22:20:57 +0300

    Example13 Fibonacci

--- Examples/Example013_RecursionAlgorithm/Program.cs
+++ Examples/Example013_RecursionAlgorithm/Program.cs
@@ -1,44 +1,70 @@
-﻿string[,] table = new string[2, 5];
-table[1, 2] = "слово";
+﻿// string[,] table = new string[2, 5];
+// table[1, 2] = "слово";
 
-for (int rows = 0; rows < 2; rows++)
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"{table[rows, columns]}");
+//     }
+// }
+
+
+
+
+// void PrintArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             Console.Write($"{matr[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// void FillArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// int[,] matrix = new int[3, 4];
+// PrintArray(matrix);
+
+// Console.WriteLine();
+
+// FillArray(matrix);
+// PrintArray(matrix);
+
+
+
+
+// double Factorial(double n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+
+// for (double i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+
+int Fibonacci(int n)
 {
-    for (int columns = 0; columns < 5; columns++)
-    {
-        Console.WriteLine($"{table[rows, columns]}");
-    }
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
-
-
-
-void PrintArray(int[,] matr)
+for (int i = 1; i < 10; i++)
 {
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            Console.Write($"{matr[i, j]} ");
-        }
-        Console.WriteLine();
-    }
+    Console.WriteLine(Fibonacci(i));
 }
-
-void FillArray(int[,] matr)
-{
-    for (int i = 0; i< matr.GetLength(0); i++)
-    {
-        for (int j = 0; j< matr.GetLength(1); j++)
-        {
-            matr[i,j] = new Random().Next(1,10);
-        }
-    }
-}
-
-int[,] matrix = new int [3, 4];
-PrintArray(matrix);
-
-Console.WriteLine();
-
-FillArray(matrix);
-PrintArray(matrix);

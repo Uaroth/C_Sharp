e311b3f90e427eb85a933415f593c582f94121d7
Sergey <melnikov_s_a@mail.ru>
2022-11-12 23:34:37 +0300

    useful methods more

--- UsefulMethods/Program.cs
+++ UsefulMethods/Program.cs
@@ -33,6 +33,22 @@ int[] RandomFill(int[] numbers)
 
 
 
+// Вычленять и суммировать цифры в числе
+// через отсечение лишних разрядов делением на 10 и модуль
+
+void SumAll(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum = sum + (number % 10);
+        number = number / 10;
+    }
+    Console.WriteLine($"Сумма цифр числа равна {sum}");
+}
+
+
+
 // Развернуть массив
 
 int [] Revert(int [] arr)

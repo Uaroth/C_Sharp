9f483c856a54283492744fe07f1ba9ead110bc16
Sergey <melnikov_s_a@mail.ru>
2022-11-22 21:46:43 +0300

    Test succesful

---
+++ TestForSpecialWork/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Здравствуйте. Введите элементы через пробел");
+string element = Console.ReadLine();
+string[] elements = element.Split(" ");
+Console.WriteLine("Элементы, в которых не больше трёх символов: ");
+for (int i = 0; i < elements.Length; i++)
+{
+    if (elements[i].Length < 4)
+    {
+        Console.Write($"{elements[i]} ");
+    }
+}

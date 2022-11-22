f415c964e9111308b27090930c4177433ccf27eb
Sergey <melnikov_s_a@mail.ru>
2022-11-22 23:04:15 +0300

    Test with array

--- TestForSpecialWork/Program.cs
+++ TestForSpecialWork/Program.cs
@@ -1,11 +1,29 @@
-﻿Console.WriteLine("Здравствуйте. Введите элементы через пробел");
-string element = Console.ReadLine();
-string[] elements = element.Split(" ");
-Console.WriteLine("Элементы, в которых не больше трёх символов: ");
-for (int i = 0; i < elements.Length; i++)
+﻿internal class Program
 {
-    if (elements[i].Length < 4)
+    private static void Main(string[] args)
     {
-        Console.Write($"{elements[i]} ");
+        Console.WriteLine("Здравствуйте. Введите элементы через пробел");
+        string element = Console.ReadLine();
+        string[] elements = element.Split(" ");
+        int count = 0;
+        for (int i = 0; i < elements.Length; i++)
+        {
+            if (elements[i].Length < 4) count++;
+        }
+
+        string[] results = new string[count];
+        int j = 0;
+
+        Console.WriteLine("Элементы, в которых не больше трёх символов:");
+        for (int i = 0; i < elements.Length; i++)
+        {
+
+            if (elements[i].Length < 4)
+            {
+                results[j] = elements[i];                
+                Console.Write($"{results[j]} ");
+                j++;
+            }
+        }
     }
 }
